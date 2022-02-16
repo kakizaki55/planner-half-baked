1d8f47f35094acb1678aa125765b63da9e4f15e5
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-16 11:06:26 -0800

    got the app working

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
+import { useEntries } from '../../context/PlannerContext';
+import Entries from '../Planner/EntryList';
 import styles from './Header.css';
 
 export default function Header() {
+  const { entries } = useEntries();
+
   return (
     <header className={styles.header}>
       <h2>My Planner</h2>
       {/* TODO: Replace hard-coded count with the actual number of entries */}
-      <span>1 Item Left</span>
+      <span>you have {entries.length} entries</span>
     </header>
   );
 }
